908218cd5fa021737340ea0b19ddd3b735373aa4
Kazuki Iwanaga <47914321+kazuki-iwanaga@users.noreply.github.com>
2023-06-19 17:46:18 +0900

    Create passworder.py

---
+++ passworder.py
@@ -0,0 +1,8 @@
+from notebook.auth import passwd
+from random import randint
+
+with open('/tmp/.env', mode='w') as f:
+    print('\n--------------------------------------------')
+    print('Please set a password for Jupyter Notebook')
+    f.write('JUPYTER_PASSWORD="'+str(passwd()).replace('$', '\$')+'"\n')
+    f.write(f'JUPYTER_PORT={randint(49152, 65535)}')
